5abe469baf72d492e51c67e391c1efa55afa399c
natalie <nataliediazrodriguez@gmail.com>
2019-08-18 16:25:44 -0400

    add wine controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/examples' => 'examples#index'
-  #   get "/photos" => "photos#index"
+    get '/wines' => 'wines#index'
   end
 
 end
