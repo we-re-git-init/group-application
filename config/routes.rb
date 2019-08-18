8c4ca2bcf2712de65a2bcfaae2d32366714614fb
Sim <simchasitzer1@gmail.com>
2019-08-18 16:20:41 -0400

    add sitzer controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
+  get '/sitzer' => 'sitzer#index'
+
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/examples' => 'examples#index'
