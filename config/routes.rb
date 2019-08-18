c6c65fcc66f97db51e8a43d798c54c5b52668274
Lexa <lexaraea@gmail.com>
2019-08-18 16:31:43 -0400

    merge issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
+  get '/sitzer' => 'sitzer#index'
+
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/examples' => 'examples#index'
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
     get '/meeae' => 'meeae#index'
 >>>>>>> 5bdee92c91e4d7a4edb55c2ef177af4071c21f34
   #   get "/photos" => "photos#index"
+    get '/shanes' => 'shanes#index'
     get '/waterbottles' => 'water_bottles#index'
   end
 
